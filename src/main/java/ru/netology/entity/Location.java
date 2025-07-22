0f062987e6cf3e37190c5a178f21a9f18a630402
Evgeny Grigorevskiy <evgeny.grigorevskiy@mail.ru>
2025-07-22 12:09:04 +0500

    g

---
+++ src/main/java/ru/netology/entity/Location.java
@@ -0,0 +1,50 @@
+package ru.netology.entity;
+
+import java.util.Objects;
+
+public class Location {
+
+    private final String city;
+
+    private final Country country;
+
+    private final String street;
+
+    private final int builing;
+
+    public Location(String city, Country country, String street, int builing) {
+        this.city = city;
+        this.country = country;
+        this.street = street;
+        this.builing = builing;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getBuiling() {
+        return builing;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return builing == location.builing && Objects.equals(city, location.city) && country == location.country && Objects.equals(street, location.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, country, street, builing);
+    }
+}
---
+++ src/main/java/ru/netology/i18n/LocalizationServiceImpl.java
@@ -0,0 +1,16 @@
+package ru.netology.i18n;
+
+import ru.netology.entity.Country;
+
+public class LocalizationServiceImpl implements LocalizationService {
+
+    public String locale(Country country) {
+        switch (country) {
+            case RUSSIA:
+                return "Добро пожаловать";
+            default:
+                return "Welcome";
+
+        }
+    }
+}
